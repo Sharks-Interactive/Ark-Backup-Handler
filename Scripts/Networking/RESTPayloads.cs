d332df21adf48062a812e2baf850abfa32f6bfb9
Shark Desktop (Owen) <owensilva1282@gmail.com>
2021-08-16 16:27:04 -0400

    Version Checking

---
+++ Scripts/Networking/RESTPayloads.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static ABH.Net.GitClient;
+
+namespace ABH.Net.Payloads
+{
+    // See https://docs.github.com/en/rest/reference/repos#releases
+    class Release
+    {
+        public string accept = "application/vnd.github.v3+json";
+        public Repository repo;
+        public int per_page;
+        public int page;
+
+        public Release(Repository _repo, int _per_page, int _page)
+        {
+            repo = _repo;
+            per_page = _per_page;
+            page = _page;
+        }
+
+        public Release(string _accept, Repository _repo, int _per_page, int _page)
+        {
+            accept = _accept;
+            repo = _repo;
+            per_page = _per_page;
+            page = _page;
+        }
+    }
+}
---
+++ Scripts/Networking/VersionHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ABH.Net
+{
+    class VersionHandler
+    {
+        public void GetLatestVersion()
+        {
+
+        }
+
+        public void ReadCurrentVersion()
+        {
+
+        }
+
+        public bool CheckCurrentVersion()
+        {
+            return false;
+        }
+    }
+}
